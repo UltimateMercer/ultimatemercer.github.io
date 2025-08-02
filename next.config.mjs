ab6c0338799f0ef28aa6b23bdfdb65c9d249c292
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2025-08-02 17:48:06 -0300

    Deploy: output

--- next.config.mjs
+++ next.config.mjs
@@ -18,8 +18,7 @@ const withMDX = createMDX({
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-
-
+  output: 'export',
   images: {
     remotePatterns: [
       {
